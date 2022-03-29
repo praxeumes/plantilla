746a592f8c9242b8a85498f3fae981b822b8a918
Pandora <pandora@iMac.local>
2022-03-28 23:52:11 -0600

    Prueba  on-line 07

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 //   userImages.push('/images/user-'+ i +'.jpg');
 // }
 
-var appShellFiles = ['/index.html'];
+var appShellFiles = ['/plantilla/index.html'];
 //var contentToCache = appShellFiles.concat(userImages);
 
 //console.log(userImages);
